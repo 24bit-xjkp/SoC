1850bfd430873617cf55d83b1e458b9f740e3348
24bit-xjkp <2283572185@qq.com>
2025-05-28 11:34:44 +0800

    feat(SoC): stm32 hal/ll内部断言支持
    1. 给c_assert_failed函数添加日志报告功能
    2. 添加gnu::noinline和gnu::cold属性

--- SoC/src/assert.cpp
+++ SoC/src/assert.cpp
@@ -2,16 +2,42 @@
 
 namespace SoC
 {
-    extern "C" [[noreturn]] void c_assert_failed() noexcept { ::SoC::fast_fail(); }
+    using namespace ::std::string_view_literals;
 
-    [[noreturn, gnu::noinline]] void assert_failed(::std::string_view message, ::std::source_location location) noexcept
+    namespace detail
+    {
+        /// 断言消息起始字符串，将终端配置为红色文字
+        constexpr auto assert_msg_start{"\N{ESCAPE}[31m文件: "sv};
+
+        /// 断言消息结束字符串，将终端文字颜色恢复默认
+        constexpr auto assert_msg_end{"\N{ESCAPE}[39m"sv};
+    }  // namespace detail
+
+    extern "C" [[noreturn]] [[using gnu: noinline, cold]]  void c_assert_failed(const char* file_name, ::std::uint32_t line, const char* function_name) noexcept
+    {
+        // 断言内容如下：
+        // 文件: file_name(line) `function_name`: message
+
+        ::SoC::println(::SoC::log_device,
+                       ::SoC::detail::assert_msg_start,
+                       ::std::string_view{file_name},
+                       "("sv,
+                       line,
+                       ") `"sv,
+                       ::std::string_view{function_name},
+                       "`: "sv,
+                       "stm32 hal/ll 内部断言失败"sv,
+                       ::SoC::detail::assert_msg_end);
+        ::SoC::fast_fail();
+    }
+
+    [[noreturn]] [[using gnu: noinline, cold]] void assert_failed(::std::string_view message, ::std::source_location location) noexcept
     {
         // 断言内容如下：
         // 文件: file_name(line:column) `function_name`: message
 
-        using namespace ::std::string_view_literals;
         ::SoC::println(::SoC::log_device,
-                       "\N{ESCAPE}[31m文件: "sv,
+                       ::SoC::detail::assert_msg_start,
                        ::std::string_view{location.file_name()},
                        "("sv,
                        location.line(),
@@ -21,7 +47,7 @@ namespace SoC
                        ::std::string_view{location.function_name()},
                        "`: "sv,
                        message,
-                       "\N{ESCAPE}[39m"sv);
+                       ::SoC::detail::assert_msg_end);
         ::SoC::fast_fail();
     }
 
--- stm32f4/hal_ll/include/stm32_assert.h
+++ stm32f4/hal_ll/include/stm32_assert.h
@@ -3,10 +3,18 @@
 #ifdef __cplusplus
 namespace SoC
 {
-    extern "C" [[noreturn]] void c_assert_failed() noexcept;
-}
+    /**
+     * @brief 适用于stm32 hal/ll库的断言接口
+     *
+     * @param file_name 文件名
+     * @param line 行号
+     * @param function_name 函数名
+     */
+    extern "C" [[noreturn]] [[using gnu: noinline, cold]] void
+        c_assert_failed(const char* file_name, ::std::uint32_t line, const char* function_name) noexcept;
+}  // namespace SoC
 #else
     #include <stdint.h>
-[[noreturn]] void c_assert_failed();
+[[noreturn]] [[gnu::noinline]] [[gnu::cold]] void c_assert_failed(const char* file_name, uint32_t line, const char* function_name);
 #endif
-#define assert_param(expr) ((expr) ? (void)0U : c_assert_failed())
+#define assert_param(expr) ((expr) ? (void)0U : c_assert_failed(__FILE__, __LINE__, __func__))
