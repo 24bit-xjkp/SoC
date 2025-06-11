acadc6d867b2120a830ff45015c211e70bd6304d
24bit-xjkp <2283572185@qq.com>
2025-06-11 20:53:29 +0800

    feat(SoC): oled支持部分控制字符和混合宽度
    1. oled::write函数支持解析\r \n \b控制字符
    2. 支持混合输出8x16和16x16字符

--- SoC/include/oled.hpp
+++ SoC/include/oled.hpp
@@ -3,6 +3,18 @@
 
 namespace SoC
 {
+    /**
+     * @brief oled字体大小
+     *
+     */
+    enum class oled_font_size : ::std::size_t
+    {
+        /// 8x16
+        f8x16,
+        /// 16x16
+        f16x16
+    };
+
     struct oled
     {
     private:
@@ -12,24 +24,104 @@ namespace SoC
         inline static ::std::array<::std::array<::std::uint8_t, 128>, 8> buffer{};
         ::SoC::i2c& i2c;
         ::SoC::dma_stream dma_stream;
+        ::std::uint8_t page{};
+        ::std::uint8_t column{};
+
+        /**
+         * @brief 将字符串写入缓冲区并刷新
+         *
+         * @tparam font_size 字体大小
+         * @param begin 字符串首指针
+         * @param end 字符串尾哨位
+         */
+        template <::SoC::oled_font_size font_size>
+        void do_write(const char* begin, const char* end) noexcept;
+
+        /**
+         * @brief 将字符串写入缓冲区并刷新
+         *
+         * @param begin 字符串首指针
+         * @param end 字符串尾哨位
+         */
+        [[using gnu: always_inline]] void write_f8x16(const char* begin, const char* end) noexcept;
+
+        /**
+         * @brief 将字符串写入缓冲区并刷新
+         *
+         * @param begin 字符串首指针
+         * @param end 字符串尾哨位
+         */
+        [[using gnu: always_inline]] void write_f16x16(const char* begin, const char* end) noexcept;
 
     public:
+        /**
+         * @brief 初始化oled显示屏
+         *
+         * @param i2c i2c外设
+         * @param dma dma数据流
+         */
         oled(::SoC::i2c& i2c, ::SoC::dma& dma) noexcept;
 
+        /**
+         * @brief 获取绑定的dma数据流
+         *
+         * @return dma数据流
+         */
         inline ::SoC::dma_stream& get_dma() noexcept { return dma_stream; }
 
+        /**
+         * @brief 获取i2c外设
+         *
+         * @return i2c外设
+         */
         inline ::SoC::i2c& get_i2c() noexcept { return i2c; }
 
+        /**
+         * @brief 写入oled命令
+         *
+         * @param command 要写入的命令
+         */
         void write_command(::std::uint8_t command) const noexcept;
 
+        /**
+         * @brief 写入oled数据
+         *
+         * @param data 要写入的数据
+         */
         void write_data(::std::uint8_t data) const noexcept;
 
-        void set_cursor(::std::uint8_t page, ::std::uint8_t column) const noexcept;
+        /**
+         * @brief 设置光标位置
+         *
+         * @param page
+         * @param column
+         */
+        void set_cursor(::std::uint8_t page, ::std::uint8_t column) noexcept;
 
-        void flush() const noexcept;
+        /**
+         * @brief 将缓冲区内容刷新到oled并清空缓冲区
+         *
+         */
+        void flush() noexcept;
 
+        /**
+         * @brief 初始化oled屏幕
+         *
+         */
         void init() noexcept;
 
-        void write(const void* begin, const void* end) const noexcept;
+        /**
+         * @brief 将字符串写入缓冲区并刷新
+         *
+         * @tparam font_size 字体大小
+         * @param begin 字符串首指针
+         * @param end 字符串尾哨位
+         */
+        template <::SoC::oled_font_size font_size = ::SoC::oled_font_size::f16x16>
+        void write(const char* begin, const char* end) noexcept
+        {
+            if constexpr(font_size == ::SoC::oled_font_size::f16x16) { write_f16x16(begin, end); }
+            else if constexpr(font_size == ::SoC::oled_font_size::f8x16) { write_f8x16(begin, end); }
+        }
     };
 }  // namespace SoC
--- SoC/src/oled.cpp
+++ SoC/src/oled.cpp
@@ -3,15 +3,20 @@
 
 namespace SoC
 {
+    using font8x16_t = ::std::array<::std::array<::std::uint8_t, 2>, 8>;
     using font16x16_t = ::std::array<::std::array<::std::uint8_t, 2>, 16>;
 
     namespace detail
     {
+        constexpr inline ::std::int8_t font8x16[]{
+#embed "../assets/font_8x16.data"
+        };
         constexpr inline ::std::int8_t font16x16[]{
-#embed "../assets/font.data"
+#embed "../assets/font_16x16.data"
         };
     }  // namespace detail
 
+    constexpr inline auto font8x16{::SoC::array_cast<font8x16_t>(::SoC::detail::font8x16)};
     constexpr inline auto font16x16{::SoC::array_cast<font16x16_t>(::SoC::detail::font16x16)};
 
     ::SoC::oled::oled(::SoC::i2c& i2c, ::SoC::dma& dma) noexcept :
@@ -36,19 +41,21 @@ namespace SoC
         i2c.write(slave_address, buffer, buffer + 2);
     }
 
-    void ::SoC::oled::set_cursor(::std::uint8_t page, ::std::uint8_t column) const noexcept
+    void ::SoC::oled::set_cursor(::std::uint8_t page, ::std::uint8_t column) noexcept
     {
-        write_command(0xb0 | page);
-        write_command(0x00 | (column & ::SoC::mask_all_zero<4>));
-        write_command(0x10 | (column >> 4));
+        this->page = page;
+        this->column = column;
     }
 
-    void ::SoC::oled::flush() const noexcept
+    void ::SoC::oled::flush() noexcept
     {
 #pragma GCC unroll(0)
         for(auto&& [page, page_index]: ::std::views::zip(buffer, ::std::views::iota(0)))
         {
-            set_cursor(page_index, 0);
+            write_command(0xb0 | page_index);
+            write_command(0x00 | 0);
+            write_command(0x10 | 0);
+
             auto _{i2c.get_condition_guard()};
             i2c.write_address(slave_address);
             i2c.write(data_prefix);
@@ -59,12 +66,17 @@ namespace SoC
                 i2c.write(column);
             }
         }
+#pragma GCC unroll(0)
+        for(auto i{0zu}; i != page; ++i) { ::std::memset(&buffer[i], 0, 128); }
+        page = 0;
+        column = 0;
     }
 
     void ::SoC::oled::init() noexcept
     {
         constexpr ::std::uint8_t commands[]{0xae, 0xd5, 0x80, 0xa8, 0x3f, 0xd3, 0x00, 0x40, 0x8d, 0x14, 0x20, 0x02, 0xa1,
                                             0xc8, 0xda, 0x12, 0x81, 0xcf, 0xd9, 0xf1, 0xdb, 0x40, 0xa4, 0xa6, 0xaf};
+#pragma GCC unroll(0)
         for(auto command: commands) { write_command(command); }
 
         // constexpr ::std::uint8_t commands[]{command_prefix, 0xae, 0xd5, 0x80, 0xa8, 0x3f, 0xd3, 0x00, 0x40,
@@ -77,20 +89,85 @@ namespace SoC
         flush();
     }
 
-    void ::SoC::oled::write(const void* begin, const void* end) const noexcept
+    template <::SoC::oled_font_size font_size>
+    void ::SoC::oled::do_write(const char* begin, const char* end) noexcept
     {
-#pragma GCC unroll(2)
-        for(auto column{0zu}; auto ch: ::std::ranges::subrange(reinterpret_cast<const ::std::uint8_t*>(begin),
-                                                               reinterpret_cast<const ::std::uint8_t*>(end)))
+#pragma GCC unroll(0)
+        for(::std::uint8_t ch: ::std::ranges::subrange(begin, end))
         {
-            auto font_index{ch - ' '};
-            for(auto&& [page0, page1]: ::SoC::font16x16[font_index])
+            constexpr bool is_f8x16{font_size == ::SoC::oled_font_size::f8x16};
+            constexpr bool is_f16x16{font_size == ::SoC::oled_font_size::f16x16};
+            constexpr auto&& table{[] static noexcept -> auto&&
+                                   {
+                                       if constexpr(is_f8x16) { return ::SoC::font8x16; }
+                                       else if constexpr(is_f16x16) { return ::SoC::font16x16; }
+                                   }()};
+
+            switch(ch)
             {
-                buffer[0][column] = page0;
-                buffer[1][column] = page1;
+                case '\b':
+                {
+                    constexpr auto column_per_ch{table.front().size()};
+                    auto temp_column{column + 128zu - column_per_ch};
+                    if(temp_column < 128) [[unlikely]]
+                    {
+                        if(page == 0) [[unlikely]] { temp_column = 0; }
+                        else
+                        {
+                            page -= 2;
+                        }
+                    }
+                    else
+                    {
+                        temp_column -= 128;
+                    }
+                    column = temp_column;
+                }
+                    continue;
+                case '\r': column = 0; continue;
+                case '\n':
+                    column = 0;
+                    page += 2;
+                    continue;
+                default: [[likely]] break;
+            }
+
+            if constexpr(is_f16x16)
+            {
+                // 行尾仅剩8列，需要换行
+                if(column + 16 > 128) [[unlikely]]
+                {
+                    column = 0;
+                    ::std::memset(&buffer[page][128], 0, 8);
+                    ::std::memset(&buffer[page + 1][128], 0, 8);
+                    page += 2;
+                }
+            }
+
+            auto font_index{::std::max(ch - ' ', 0)};
+#pragma GCC unroll(2)
+            for(auto&& [page0, page1]: table[font_index])
+            {
+                buffer[page][column] = page0;
+                buffer[page + 1][column] = page1;
                 ++column;
+            }
+            if(column == 128) [[unlikely]]
+            {
+                page += 2;
+                column = 0;
             }
         }
         flush();
+    }
+
+    void ::SoC::oled::write_f8x16(const char* begin, const char* end) noexcept
+    {
+        do_write<::SoC::oled_font_size::f8x16>(begin, end);
+    }
+
+    void ::SoC::oled::write_f16x16(const char* begin, const char* end) noexcept
+    {
+        do_write<::SoC::oled_font_size::f16x16>(begin, end);
     }
 }  // namespace SoC
