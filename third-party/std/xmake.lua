97e9ef59b64837eac6dfc2c2e2ad01594406800d
24bit-xjkp <2283572185@qq.com>
2025-09-15 00:44:23 +0800

    build(xmake): 添加带单元测试目标的注册接口
    1. 添加register_target_with_unit_test用于注册带单元测试的目标，自动为单元测试添加宿主平台编译选项，同时支持为主目标和单元测试目标分别定制参数
    2. 使用register_target_with_unit_test重构SoC.std和SoC.freestanding目标的注册流程

--- third-party/std/xmake.lua
+++ third-party/std/xmake.lua
@@ -1,4 +1,4 @@
-target("SoC.std")
+register_target_with_unit_test("SoC.std", function ()
     set_kind("object")
     if string.find(get_config("toolchain") or "", "gcc") then
         add_files("gcc.cppm", { public = true })
@@ -12,23 +12,4 @@ target("SoC.std")
             target:set("policy", "build.optimization.lto", false)
         end
     end)
-target_end()
-
-target("SoC.std.unit_test")
-    set_kind("object")
-    set_arch(os.arch())
-    set_plat(get_config("host"))
-    set_enabled(get_config("build_unit_test"))
-    if string.find(get_config("toolchain") or "", "gcc") then
-        add_files("gcc.cppm", { public = true })
-    else
-        add_files("clang.cppm", { public = true })
-    end
-
-    on_load(function (target)
-        import("utility.common")
-        if common.is_gcc() then
-            target:set("policy", "build.optimization.lto", false)
-        end
-    end)
-target_end()
+end)
