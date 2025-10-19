4b6771695b3f970da650976f834ff49294e27edb
24bit-xjkp <2283572185@qq.com>
2025-10-19 23:51:04 +0800

    build(xmake): 将SoC.freestanding.unit_test设置为object
    1. SoC.freestanding保持object以便和SoC.stm32一起组成一个静态库而不需要额外的merge
    2. 由于Windows下dll需要包含全部符号，因此将SoC.freestanding.unit_test设置为object

--- libSoC/freestanding/xmake.lua
+++ libSoC/freestanding/xmake.lua
@@ -8,11 +8,14 @@ register_target_with_unit_test("SoC.freestanding", function ()
             target:set("policy", "build.optimization.lto", false)
         end
     end)
+    -- 对于嵌入式平台，设置为对象库以便和SoC.stm32合并生成一个静态库
+    -- 由于静态库merge需要多开启一个ar进程，这在Windows下较慢，因此不采用静态库合并
+    -- 对于单元测试，Windows下dll内必须实现全部函数，而SoC::assert_failed等函数需要外部实现
+    -- 因此也设置为对象库
+    set_kind("object")
 end, function ()
     add_deps("SoC.std")
-    set_kind("object")
 end, function ()
     add_deps("SoC.std.unit_test")
     add_defines("SOC_IN_UNIT_TEST", {public = true})
-    set_kind("shared")
 end)
